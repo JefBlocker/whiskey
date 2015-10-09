cba7fe2ab35ba1e317d33a6bbfa2d92c1fe2e97c
Jef Blocker <jefblocker@gmail.com>
2015-10-09 14:02:06 -0400

    Added Javascript to insert etsy.js data into index.html

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 (function () {
 
-var templateString = $('#itemTemplate').text();
-var itemFunction = _.template(templateString);
+    // Grab the template string.
+    var templateString = $('#item-template').text();
 
-_.each(etsy.results, function (item) {
-  console.log(item);
-  var itemHTML = templateFunction(item);
-  $('.container').append(itemHTML);
-});
+    // Turn it into a template function.
+    var renderTemplate = _.template(templateString);
 
+    _.each(etsy.results, function(item) {
 
+    // Pass in an object. Return value is a string
+    // with the bee stings replaced with object's properties
+    var freshHTML = renderTemplate(item);
+
+    // Inject the fresh html into the page.
+    $('.container').append(freshHTML);
+  });
 
 }());
+
+//var templateString = $('#itemTemplate').text();
+//var itemFunction = _.template(templateString);
+
+//_.each(etsy.results, function (item) {
+ // console.log(item);
+ // var itemHTML = templateFunction(item);
+ // $('.container').append(itemHTML);
+//});
